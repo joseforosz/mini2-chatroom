d89938346e0fbb3fb00d1952e6607d46d265df30
Joe Orosz <joseforosz@gmail.com>
2015-03-10 11:22:22 -0600

    initial commit

---
+++ app/AddChat.js
@@ -0,0 +1,18 @@
+var React = require('react');
+var $ = require('jquery');
+
+var AddChat = React.createClass({
+  propTypes: {
+    url: React.PropTypes.string.isRequired
+  },
+  
+
+  render: function(){
+    return (
+      <div className="form-group">
+      </div>
+    )
+  }
+});
+
+module.exports = AddChat;
